e4c34d3880bf7b533264fee5fa2c91b92a887bb5
ConradKotz <conbkotz@gmail.com>
2026-03-26 15:06:04 +1100

    Pre boot menu scene commit (everything working)

---
+++ Assets/Scripts/SaveLoad/SaveGameHeader.cs
@@ -0,0 +1,12 @@
+[System.Serializable]
+public class SaveGameHeader
+{
+    public int slotIndex;
+    public bool hasSave;
+    public string characterName;
+    public int playerLevel;
+    public int gold;
+    public string sceneName;
+    public string lastSavedUtc;
+    public float playTimeSeconds;
+}
